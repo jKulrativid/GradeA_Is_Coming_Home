eac19b32a9a8fb93194ba9c04599b58554dea906
K. Jetnipit <jetnipit54@gmail.com>
2023-04-22 17:14:18 +0700

    almost there v2

--- powergrid/main.cpp
+++ powergrid/main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <chrono>
-#include <cstdlib>
 #include <filesystem>
-#include <sstream>
-#include <string>
 #include <fstream>
-#include <omp.h>
-#include <algorithm>
 #include <stdio.h>
 
 extern "C" {
@@ -15,21 +9,6 @@ extern "C" {
 }
 
 #define answer std::pair<int, std::string>
-
-int N_THREAD;
-int N_PREFIX;
-
-void _calc_prefix()
-{
-  int n = N_THREAD + 1;
-  N_PREFIX = 0;
-  while (1)
-  {
-    n >>= 1;
-    if (!n) break;
-    N_PREFIX++;
-  }
-}
 
 void readfile(const std::string& filename, int& G, std::vector<std::vector<int>>& E)
 {
@@ -44,57 +23,54 @@ void readfile(const std::string& filename, int& G, std::vector<std::vector<int>>
   getline(fin, line);
   fin >> nE;
   int i = 0;
-  E.resize(nE);
+  E.resize(G);
   int a, b;
   while (i < nE)
   {
       getline(fin, line);
       fin >> a >> b;
-      E[i].resize(2);
-      E[i][0] = a; E[i][1] = b;
+      E[a].push_back(b);
+      E[b].push_back(a);
       i++;
   }
 }
 
-answer _solve_vertex_cover(const int& G, const std::vector<std::vector<int>>& E,  const int& tid) {
+answer _solve_vertex_cover(const int& G, const std::vector<std::vector<int>>& E) {
   lprec* lp;
   lp = make_lp(0, G);
-  set_verbose(lp, IMPORTANT);
+  set_verbose(lp, SEVERE);
 
-  double row[G+1]; int col[G+1];
+  REAL row[G+1];
   for (int i = 1; i <= G; i++)
   {
     char buffer[100];
-    col[i] = i;
     sprintf(buffer, "x-%d", i);
     set_col_name(lp, i, buffer);
-    set_int(lp, i, TRUE);
+    set_binary(lp, i, TRUE);
   }
 
-  for (int i = 1; i <= G; i++)
+  for (int i = 0; i <= G; i++)
   {
     row[i] = 0;
   }
 
-  for (int i = 0; i < E.size(); i++)
+  int cnt = 0;
+  int colno[G+1];
+  for (int i = 0; i < G; i++)
   {
-    row[E[i][0]+1] = 1; row[E[i][1]+1] = 1;
-    add_constraint(lp, row, GE, 1);
-    row[E[i][0]+1] = 0; row[E[i][1]+1] = 0;
+    for (auto& e: E[i])
+    {
+      row[cnt] = 1;
+      colno[cnt++] = e+1;
+    }
+    row[cnt] = 1; colno[cnt++] = i+1;
+    add_constraintex(lp,cnt, row,colno, GE, 1);
+    for (int i = 0; i <= G; i++)
+    {
+      row[i] = 0;
+    }
+    cnt = 0;
   }
-
-  for (int i = 1; i <= G; i++)
-  {
-    row[i] = 1;
-    add_constraint(lp, row, GE, 0);
-    row[i] = -1;
-    add_constraint(lp, row, GE, -1);
-    row[i] = 0;
-  }
-
-  // MORE CONSTRAINT DUE TO THREAD
-
-  // END MORE CONSTRAINT
 
   set_add_rowmode(lp, FALSE);
 
@@ -105,15 +81,17 @@ answer _solve_vertex_cover(const int& G, const std::vector<std::vector<int>>& E,
 
   set_obj_fn(lp, row);
   set_minim(lp);
-  
-  int ret = solve(lp);
 
-  get_variables(lp, row);
+  int ret = solve(lp);
+  if (ret == 2) return std::make_pair(G+10000, "");
+
+  REAL ans[G];
+  get_variables(lp, ans);
 
   std::string s = ""; int n;
   for (int i = 0; i < G; i++)
   {
-    if (row[i] == 1)
+    if (ans[i] == 1)
     {
       s += "1";
       n++;
@@ -129,25 +107,6 @@ answer _solve_vertex_cover(const int& G, const std::vector<std::vector<int>>& E,
   return std::make_pair(n, s);
 }
 
-answer* solve_vertex_cover(const int& G, const std::vector<std::vector<int>>& E)
-{
-  std::vector<answer> ans(N_THREAD);
-  #pragma omp parallel for
-  for (int t = 0; t < N_THREAD; t++)
-  {
-    ans[t] = _solve_vertex_cover(std::ref(G), std::ref(E), std::ref(t));
-  }
-
-  answer tempbest = { .n = G+1000, .seq = "" };
-  answer* best = &tempbest;
-  for (int t = 0; t < N_THREAD; t++)
-  {
-    
-  }
-
-  return best;
-}
-
 int main(int argc, char** argv)
 {
   std::ios_base::sync_with_stdio(false); std::cin.tie(NULL);
@@ -156,27 +115,18 @@ int main(int argc, char** argv)
 		std::cout << "missing args\n";
 		exit(1);
 	}
-	char* nthreadChar = getenv("NTHREAD");
-	if (!nthreadChar)
-	{
-		nthreadChar = "12";
-	}
-	N_THREAD = atoi(nthreadChar);
-	omp_set_num_threads(N_THREAD);
-  
-	std::cout << "N THread = " << N_THREAD << std::endl;
-
   std::string inputpath(argv[1]);
   std::string outputpath(argv[2]);
-  std::cout << "I/0 : " << inputpath << "|" << outputpath << std::endl;
-
-  _calc_prefix();
-
-	std::cout << "--- SETUP COMPLETED ---\n";
 
   int G = 0;
   std::vector<std::vector<int>> E;
   readfile(inputpath, G, E);
 
-  solve_vertex_cover(G, E);
+  auto ans = _solve_vertex_cover(G, E);
+
+  std::ofstream fout(outputpath);
+  
+  fout << ans.first << ":" << ans.second;
+
+  fout.close();
 }
